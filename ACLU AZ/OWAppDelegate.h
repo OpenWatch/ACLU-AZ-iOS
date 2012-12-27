1bdecd32104106da0858858a4b981557e2751a55
Chris Ballinger <chrisballinger@gmail.com>
2012-12-27 14:07:30 -0500

    Initial Commit

---
+++ ACLU AZ/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  ACLU AZ
+//
+//  Created by Christopher Ballinger on 12/27/12.
+//  Copyright (c) 2012 OpenWatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ACLU AZ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ACLU AZ
+//
+//  Created by Christopher Ballinger on 12/27/12.
+//  Copyright (c) 2012 OpenWatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OWAppDelegate class]));
+    }
+}
