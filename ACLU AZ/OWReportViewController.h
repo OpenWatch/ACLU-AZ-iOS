f72c7f02d3e972ff022b80483bebb9b827f48cf0
Chris Ballinger <chrisballinger@gmail.com>
2012-12-27 15:15:57 -0500

    Add report view

---
+++ ACLU AZ/OWReportViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWReportViewController.h
+//  ACLU AZ
+//
+//  Created by Christopher Ballinger on 12/27/12.
+//  Copyright (c) 2012 OpenWatch FPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWReportViewController : UIViewController
+
+@end
---
+++ ACLU AZ/OWReportViewController.m
@@ -0,0 +1,38 @@
+//
+//  OWReportViewController.m
+//  ACLU AZ
+//
+//  Created by Christopher Ballinger on 12/27/12.
+//  Copyright (c) 2012 OpenWatch FPC. All rights reserved.
+//
+
+#import "OWReportViewController.h"
+
+@interface OWReportViewController ()
+
+@end
+
+@implementation OWReportViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
