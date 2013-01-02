50cde1ef847259c5a3358c7e75c7852a0537f553
Chris Ballinger <chrisballinger@gmail.com>
2013-01-02 13:51:35 -0800

    User info form done

--- ACLU AZ/OWUserInfoViewController.m
+++ ACLU AZ/OWUserInfoViewController.m
@@ -14,6 +14,14 @@
 #define USER_INFO_FILENAME @"userinfo.json"
 #define LAST_NAME_KEY @"last_name"
 #define FIRST_NAME_KEY @"first_name"
+#define ADDRESS_1_KEY @"address_1"
+#define ADDRESS_2_KEY @"address_2"
+#define CITY_KEY @"city"
+#define STATE_KEY @"state"
+#define ZIP_KEY @"zip"
+#define EMAIL_KEY @"email"
+#define PHONE_KEY @"phone"
+#define ALTERNATE_KEY @"alternate"
 
 @interface OWUserInfoViewController ()
 
@@ -104,7 +112,9 @@
     root.title = USER_INFO_STRING;
     root.controllerName = NSStringFromClass([self class]);
     root.grouped = YES;
-	QSection *infoSection = [[QSection alloc] init];
+	QSection *nameSection = [[QSection alloc] initWithTitle:NAME_STRING];
+    QSection *addressSection = [[QSection alloc] initWithTitle:ADDRESS_STRING];
+    QSection *otherSection = [[QSection alloc] initWithTitle:OTHER_STRING];
     
     QEntryElement *firstNameElement = [[QEntryElement alloc] initWithTitle:FIRST_NAME_STRING Value:nil Placeholder:REQUIRED_STRING];
     firstNameElement.key = FIRST_NAME_KEY;
@@ -113,10 +123,55 @@
     lastNameElement.key = LAST_NAME_KEY;
     lastNameElement.bind = [OWACLUAZUtilities textValueBindingForKey:LAST_NAME_KEY];
     
-    [infoSection addElement:firstNameElement];
-    [infoSection addElement:lastNameElement];
+    [nameSection addElement:firstNameElement];
+    [nameSection addElement:lastNameElement];
     
-    [root addSection:infoSection];
+    QEntryElement *address1Element = [[QEntryElement alloc] initWithTitle:[ADDRESS_STRING stringByAppendingString:@" 1"] Value:nil];
+    address1Element.key = ADDRESS_1_KEY;
+    address1Element.bind = [OWACLUAZUtilities textValueBindingForKey:ADDRESS_1_KEY];
+    
+    QEntryElement *address2Element = [[QEntryElement alloc] initWithTitle:[ADDRESS_STRING stringByAppendingString:@" 2"] Value:nil];
+    address2Element.key = ADDRESS_2_KEY;
+    address2Element.bind = [OWACLUAZUtilities textValueBindingForKey:ADDRESS_2_KEY];
+    
+    QEntryElement *cityElement = [[QEntryElement alloc] initWithTitle:CITY_STRING Value:nil];
+    cityElement.key = CITY_KEY;
+    cityElement.bind = [OWACLUAZUtilities textValueBindingForKey:CITY_KEY];
+    
+    QEntryElement *stateElement = [[QEntryElement alloc] initWithTitle:STATE_STRING Value:nil];
+    stateElement.key = STATE_KEY;
+    stateElement.bind = [OWACLUAZUtilities textValueBindingForKey:STATE_KEY];
+    
+    QEntryElement *zipElement = [[QEntryElement alloc] initWithTitle:ZIP_CODE_STRING Value:nil];
+    zipElement.key = ZIP_KEY;
+    zipElement.bind = [OWACLUAZUtilities textValueBindingForKey:ZIP_KEY];
+    zipElement.keyboardType = UIKeyboardTypeNumberPad;
+    
+    [addressSection addElement:address1Element];
+    [addressSection addElement:address2Element];
+    [addressSection addElement:cityElement];
+    [addressSection addElement:stateElement];
+    [addressSection addElement:zipElement];
+    
+    QEntryElement *emailElement = [[QEntryElement alloc] initWithTitle:EMAIL_STRING Value:nil];
+    emailElement.key = EMAIL_KEY;
+    emailElement.bind = [OWACLUAZUtilities textValueBindingForKey:EMAIL_KEY];
+    emailElement.keyboardType = UIKeyboardTypeEmailAddress;
+    QEntryElement *phoneElement = [[QEntryElement alloc] initWithTitle:PHONE_STRING Value:nil];
+    phoneElement.key = PHONE_KEY;
+    phoneElement.bind = [OWACLUAZUtilities textValueBindingForKey:PHONE_KEY];
+    phoneElement.keyboardType = UIKeyboardTypeNumberPad;
+    QEntryElement *alternateElement = [[QEntryElement alloc] initWithTitle:ALTERNATE_STRING Value:nil];
+    alternateElement.key = ALTERNATE_KEY;
+    alternateElement.bind = [OWACLUAZUtilities textValueBindingForKey:ALTERNATE_KEY];
+    
+    [otherSection addElement:emailElement];
+    [otherSection addElement:phoneElement];
+    [otherSection addElement:alternateElement];
+    
+    [root addSection:nameSection];
+    [root addSection:addressSection];
+    [root addSection:otherSection];
     
     return root;
 }
